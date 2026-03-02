cbc1d7bc283d9216fa4bc174081f32580efd0fa5
Miles Smith <miles-smith@omrf.org>
2026-03-02 14:42:02 -0600

    reformatting, set dep versions, code cleanup, remove loompy, removing some star imports

---
+++ src/mofapy2/build_model/train_model.py
@@ -0,0 +1,29 @@
+"""
+Module to train a bioFAM model
+"""
+
+
+from mofapy2.core.BayesNet import BayesNet
+
+
+def train_model(model):
+    # Sanity check on the Bayesian Network
+    assert isinstance(model, BayesNet), "'model' has to be a BayesNet class"
+
+    ####################
+    ## Start training ##
+    ####################
+
+    print("\n")
+    print("#" * 38)
+    print("## Training the model with seed %d ##" % (model.options["seed"]))
+    print("#" * 38)
+    print("\n")
+
+    model.iterate()
+
+    print("\n")
+    print("#" * 23)
+    print("## Training finished ##")
+    print("#" * 23)
+    print("\n")
